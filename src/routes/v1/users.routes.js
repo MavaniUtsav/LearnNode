312b4a1bd26adbab25e484b5abef6a3c96efacbe
srushti26794 <srushtichangela267@gmail.com>
2023-12-28 18:03:30 +0530

    Done

--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -9,28 +9,39 @@ const countVisit = (req,res,next) => {
     next()
 }
 
-router.route('/')
-    .all(countVisit)
-    .get((req, res) => {
-        res.send('Get Users api ' + "called " + visitCount + " times")0
-    })
-    .post((req, res) => {
-        res.send('Post Users api')
-    })
-    .put((req, res) => {
-        res.send('Put Users api')
-    })
-    .delete((req, res) => {
-        res.send('Delete Users api')
-    })
+// router.route('/')
+//     .all(countVisit)
+//     .get((req, res) => {
+//         res.send('Get Users api ' + "called " + visitCount + " times")
+//     })
+//     .post((req, res) => {
+//         res.send('Post Users api')
+//     })
+//     .put((req, res) => {
+//         res.send('Put Users api')
+//     })
+//     .delete((req, res) => {
+//         res.send('Delete Users api')
+//     })
 
-router.get('/new', (req, res) => {
-    res.send(`Get new users api`)
-})
+// router.get('/new', (req, res) => {
+//     res.send(`Get new users api`)
+// })
+
+router.param("name", (req, res, next, id) => {
+    console.log(id);
+    next();
+});
+
+// router.get('/:name',(req,res) => {
+//     res.send(`Get users api ${req.params.name}`)
+// })
 
 router.get('/:id', (req, res) => {
     res.send(`Get users api ${req.params.id}`)
 })
+
+
 
 // router.get('/',countVisit,(req, res) => {
 //     res.send('Get Users api ' + "called " + visitCount + " times")
