91a59daaa0881377ace278b85e5f999e611b6bbe
MavaniUtsav <utsavmvani01@gmail.com>
2024-01-11 19:17:56 +0530

    Done

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -1,10 +1,11 @@
 const Category = require("../model/category.model");
+const { categoryService } = require("../services");
 const { createCategoryService, getCategoryService } = require("../services/category.service");
 
 
 const createCategory = async (req,res) => {
     try {
-        let category = await Category(createCategoryService(req.body))
+        let category = await categoryService.createCategory(req.body)
 
         if (!category) {
             throw new Error('Create category error:')
@@ -21,21 +22,59 @@ const createCategory = async (req,res) => {
 
 const getCategory = async (req,res) => {
     try {
-        let category = await getCategoryService()
+        let category = await categoryService.getCategory()
         console.log(category);
         if (!category) {
-            throw new Error('Create category error:')
+            throw new Error('Get category error:')
         }
 
         res.status(200).json({
-            success: true
+            success: true,
+            data: category
         })
     } catch (error) {
+        console.log(error.message);
         
+    }
+}
+
+const deleteCategory = async (req,res) => {
+    try {
+        let category = await categoryService.deletecategory(req.params.id)
+
+        if (!category) {
+            throw new Error('Delete category error:')
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'Category deleted!!'
+        })
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const updateCategory = async (req,res) => {
+    try {
+        let category = await categoryService.updateCategory(req.query.id,req.body)
+        console.log(category);
+        if (!category) {
+            throw new Error('Update category error:')
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'Category Updated!!',
+        })
+    } catch (error) {
+        console.log(error.message);
     }
 }
 
 module.exports = {
     createCategory,
-    getCategory
+    getCategory,
+    deleteCategory,
+    updateCategory
 }
