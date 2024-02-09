2af68fa51a3d8f806439fc6c1b7e94f4f849fcf3
MavaniUtsav <utsavmvani01@gmail.com>
2024-02-09 19:56:40 +0530

    Done

--- src/utils/pdfMake.js
+++ src/utils/pdfMake.js
@@ -8,10 +8,10 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
 const fontsDirectory = path.join(__dirname, 'src', 'fonts');
 pdfMake.fonts = {
     Roboto: {
-        normal: path.join(fontsDirectory , 'Roboto-Regular.ttf'),
-        bold: path.join(fontsDirectory , 'Roboto-Medium.ttf'),
-        italics: path.join(fontsDirectory , 'Roboto-Italic.ttf'),
-        bolditalics: path.join(fontsDirectory , 'Roboto-MediumItalic.ttf')
+        normal: path.join(fontsDirectory, 'Roboto-Regular.ttf'),
+        bold: path.join(fontsDirectory, 'Roboto-Medium.ttf'),
+        italics: path.join(fontsDirectory, 'Roboto-Italic.ttf'),
+        bolditalics: path.join(fontsDirectory, 'Roboto-MediumItalic.ttf')
     },
 }
 const image = fs.readFileSync('./src/temp/2b34181f-bc15-42d8-98cb-4d26099afbdb1705304702492HMBoysLong-SleevedCottonShirts1.jpg', 'base64')
@@ -35,7 +35,7 @@ const pdfDefinition = {
             width: 200,
         }
     ],
-    defaultStyle:   {
+    defaultStyle: {
         font: 'Roboto'
     }
 };
@@ -43,18 +43,7 @@ const pdfDefinition = {
 const makePdf = async (req, res) => {
     console.log('makePDF');
     try {
-        const pdfDoc = pdfMake.createPdf(pdfDefinition).getBase64();
-
-         // Wrap the getBase64 function in a Promise
-         const base64 = await new Promise((resolve, reject) => {
-            pdfDoc.getBase64((result) => {
-                console.log('resolve');
-                resolve(result);
-            }, (error) => {
-                console.log('reject');
-                reject(error);
-            });
-        });
+        const base64 = await pdfMake.createPdf(pdfDefinition).getBase64();
 
         // Set appropriate headers for PDF response
         res.setHeader('Content-Type', 'application/pdf');
@@ -71,10 +60,23 @@ const makePdf = async (req, res) => {
         console.error(error.stack);
 
         res.status(500).json({
-            message: 'Internal error: ' + error.message
+            message: 'Internal error: ' + (error.message || 'Unknown error'),
+            error: error.stack
         });
     }
 
 }
 
 module.exports = makePdf
+
+
+// Wrap the getBase64 function in a Promise
+//  const base64 = await new Promise((resolve, reject) => {
+//     pdfDoc.getBase64((result) => {
+//         console.log('resolve');
+//         resolve(result);
+//     }, (error) => {
+//         console.log('reject');
+//         reject(error);
+//     });
+// });
