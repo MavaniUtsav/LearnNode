c3cba556a6804cfc9aafe0532017d224aad02feb
MavaniUtsav <utsavmvani01@gmail.com>
2024-02-13 19:48:45 +0530

    Done

--- src/utils/socket.io.js
+++ src/utils/socket.io.js
@@ -10,9 +10,26 @@ const connetcSocket = () => {
         }
      });
 
-    io.on("connection", (socket) => {
-        // console.log(socket.id);
-        socket.emit('send-msg', 'Hello World!!')
+     io.on("connection", (socket) => {
+        console.log(`User connected: ${socket.id}`);
+
+        // Handle incoming messages from the client
+        socket.on('message', (message) => {
+            console.log(`Received message from ${socket.id}: ${message}`);
+        });
+
+        socket.on("text", (obj) => {
+            io.to(obj.chatId).emit('message', obj.message);
+        })
+
+        socket.on('join_group', (groupId) => {
+            socket.join(groupId);
+        })
+
+        // Handle disconnection
+        socket.on('disconnect', () => {
+            console.log(`User disconnected: ${socket.id}`); 
+        });
     });
 
     io.listen(4000);
